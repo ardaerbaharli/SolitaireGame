aed774c155c4840565053d60692d297509fb575f
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-09-24 11:11:54 +0300

    take screenshots

---
+++ Assets/Script/Screenshot.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screenshot : MonoBehaviour
+{
+    private int ssIndex;
+    //here you can set the folder you want to use, 
+    //IMPORTANT - use "@" before the string, because this is a verbatim string
+    //IMPORTANT - the folder must exists
+    string pathToYourFile = @"D:\Random projects\Solitaire\";
+    //this is the name of the file
+    string fileName = "ss";
+    //this is the file type
+    string fileType = ".png";
+
+
+    private void Awake()
+    {        
+        ssIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F10))
+        {
+            UnityEngine.ScreenCapture.CaptureScreenshot(pathToYourFile + fileName + ssIndex + fileType);
+            ssIndex++;
+        }
+    }
+}
